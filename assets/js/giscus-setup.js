3cd281105876e56f206e955755c1ac49a491c7b4
karkman <antti.karkman@helsinki.fi>
2025-11-02 14:53:17 +0000

    Deploying to gh-pages from @ karkman/karkman.github.io@62f2cb5ccbf24fc3843bf7879a4844f9ec908edc 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "karkman/webpages",
+    "data-repo": "karkman/karkman.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
